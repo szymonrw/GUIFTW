54fcc7b0f6641a6384fd0c3080e01865257e8117
Szymon Witamborski <santamon@gmail.com>
2011-03-19 15:21:42 +0100

    Changed project description a bit.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject guiftw "0.1.0-SNAPSHOT"
-  :description "Abstract bridge for building GUIs in Swing and SWT."
+  :description "Declarative GUI framework with Swing and SWT backends."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
