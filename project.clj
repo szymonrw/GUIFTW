915e4e1a275258caa37dac5070cb22063bd4ec0b
Szymon Witamborski <santamon@gmail.com>
2011-04-22 17:48:12 +0200

    Bumped version to 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject guiftw "0.1.0-SNAPSHOT"
+(defproject guiftw "0.2.0-SNAPSHOT"
   :description "Declarative GUI framework with Swing and SWT backends."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
