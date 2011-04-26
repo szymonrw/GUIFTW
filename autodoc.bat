dc34a598e30311274eef2b1bbf118e17f0993234
Szymon Witamborski <santamon@gmail.com>
2011-04-26 21:29:51 +0200

    Autodoc commit.

--- autodoc.bat
+++ autodoc.bat
@@ -2,4 +2,7 @@
 @rem set GOODOLDPATH=%PATH%
 @rem set PATH=C:\Native\Git\bin;C:\Java\JRE\bin
 java -cp "../../autodoc/autodoc-0.8.0-SNAPSHOT-standalone.jar;../src;../lib/dev/swt-win32-win32-x86_64-3.5.2.jar" autodoc.autodoc --name "GUI FTW!" --description "Declarative GUI framework for Clojure" --external-doc-tmpdir tmp --copyright "2011 Szymon Witamborski, MIT Licence" --root .. --output-path .
+git add -A
+git commit -m "Automatic docs update."
+git push origin gh-pages
 @rem set PATH=%GOODOLDPATH%
