6c118deed636047d37e6afa1ff582c22f233a267
Szymon Witamborski <santamon@gmail.com>
2011-04-26 17:25:22 +0200

    Changes in generation script

--- autodoc.bat
+++ autodoc.bat
@@ -1,3 +1,1 @@
-cd ..
-java -cp "../autodoc-jukka/autodoc-0.8.0-SNAPSHOT-standalone.jar;src;lib/dev/swt-win32-win32-x86_64-3.5.2.jar" autodoc.autodoc --name "GUI FTW!" --description "Declarative GUI framework for Clojure" --web-src-dir http://github.com/santamon/GUIFTW/blob/ --external-doc-tmpdir autodoc/tmp --copyright "(c)2011 Szymon Witamborski, MIT Licence"
-cd autodoc
+java -cp "../../autodoc/autodoc-0.8.0-SNAPSHOT-standalone.jar;../src;../lib/dev/swt-win32-win32-x86_64-3.5.2.jar" autodoc.autodoc --name "GUI FTW!" --description "Declarative GUI framework for Clojure" --web-src-dir "http://github.com/santamon/GUIFTW/blob/" --external-doc-tmpdir tmp --copyright "2011 Szymon Witamborski, MIT Licence" --root .. --output-path .
