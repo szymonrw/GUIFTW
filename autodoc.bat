f65813c321b9f82756b991dfb4f2e2780fa94b30
Szymon Witamborski <santamon@gmail.com>
2011-04-26 17:39:50 +0200

    Fixed docs generation.

--- autodoc.bat
+++ autodoc.bat
@@ -1,1 +1,5 @@
+rem asdf
+set GOODOLDPATH=%PATH%
+set PATH=C:\Native\Git\bin;C:\Java\JRE\bin
 java -cp "../../autodoc/autodoc-0.8.0-SNAPSHOT-standalone.jar;../src;../lib/dev/swt-win32-win32-x86_64-3.5.2.jar" autodoc.autodoc --name "GUI FTW!" --description "Declarative GUI framework for Clojure" --web-src-dir "http://github.com/santamon/GUIFTW/blob/" --external-doc-tmpdir tmp --copyright "2011 Szymon Witamborski, MIT Licence" --root .. --output-path .
+set PATH=%GOODOLDPATH%
