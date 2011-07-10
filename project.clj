99ce27a1b126e2463299ceb02310ee14743a8012
Szymon Witamborski <santamon@gmail.com>
2011-07-10 15:05:53 +0200

    Rather than depending on SWT in :dev-dependencies, put manually swt.jar in project dir.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Declarative GUI framework with Swing and SWT backends."
   :dependencies [[org.clojure/clojure "[1.2,2.0)"]]
   :aot [#"guiftw\.*"]
-  :dev-dependencies [[org.eclipse/swt-win32-win32-x86_64 "3.5.2"]])
+  ;; put your swt.jar in project's directory
+  :extra-classpath-dirs ["swt.jar"])
