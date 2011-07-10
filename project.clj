2a4c4f41ef855435bdf70c18d064a7d793b07e8d
Szymon Witamborski <santamon@gmail.com>
2011-07-10 14:55:24 +0200

    Not depending on swank-clojure any more. Use `lein plugin install swank-clojure 1.3.1` instead.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "Declarative GUI framework with Swing and SWT backends."
   :dependencies [[org.clojure/clojure "[1.2,2.0)"]]
   :aot [#"guiftw\.*"]
-  :dev-dependencies [[swank-clojure "1.3.0"]
-		     [org.eclipse/swt-win32-win32-x86_64 "3.5.2"]])
+  :dev-dependencies [[org.eclipse/swt-win32-win32-x86_64 "3.5.2"]])
